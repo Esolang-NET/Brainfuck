522a6aa34cc4ac3fed777c81debacb8536bd332a
juner <juner@juner.net>
2023-10-13 08:15:06 +0900

    add consoleappframeworks

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var builder = ConsoleApp.CreateBuilder(args);
+builder.ConfigureServices(v =>
+{
+
+});
+
+var app = builder.Build();
+
+await app.RunAsync();
