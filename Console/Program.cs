817a79deb5d23bca9157b911a295b677732ad645
juner <juner@juner.net>
2023-10-17 08:16:55 +0900

    fix

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Brainfuck;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
