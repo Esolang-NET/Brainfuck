e9fcc510747b1dd097f48bf31e8ae1a7c48d3886
juner <juner@juner.net>
2026-05-27 00:17:27 +0900

    dotnet-brainfuck 周りのテストの実装の調整の完了

--- Interpreter.Tests/ProgramTests.cs
+++ Interpreter.Tests/ProgramTests.cs
@@ -5,26 +5,24 @@ namespace Esolang.Brainfuck.Interpreter.Tests;
 [TestClass]
 public class ProgramTests
 {
-    [TestMethod]
-    public void RunAsync_ParseCommand_ReturnsZero()
+    static int Run(string[] args)
     {
         var entryPoint = typeof(Program).Assembly.EntryPoint!;
-
-        object?[] parmaeters = [new string[] { "parse", "++" }];
+        Assert.IsNotNull(entryPoint);
+        object?[] parmaeters = [args];
         var result = entryPoint.Invoke(null, parmaeters) as int?;
         Assert.IsNotNull(result);
-        var exitCode = result.Value;
-        Assert.AreEqual(0, exitCode);
+        return result.Value;
     }
+    [TestMethod]
+    public void RunAsync_ParseCommand_ReturnsZero() => Assert.AreEqual(0, Run(["parse", "++"]));
 
     [TestMethod]
-    public void RunAsync_DefaultCommand_ReturnsZero()
-    {
-        var entryPoint = typeof(Program).Assembly.EntryPoint!;
-        object?[] parmaeters = [new string[] { "++++" }];
-        var result = entryPoint.Invoke(null, parmaeters) as int?;
-        Assert.IsNotNull(result);
-        var exitCode = result.Value;
-        Assert.AreEqual(0, exitCode);
-    }
+    public void RunAsync_DefaultCommand_ReturnsZero() => Assert.AreEqual(0, Run(["+++"]));
+
+    [TestMethod]
+    public void RunAsync_EmptyArgs_ReturnOne() => Assert.AreEqual(1, Run([]));
+
+    [TestMethod]
+    public void RunAsync_Help_ReturnZero() => Assert.AreEqual(0, Run(["--help"]));
 }
--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,15 +1,32 @@
 using Esolang.Brainfuck.Interpreter;
 using System.CommandLine;
 
-RootCommand rootCommand = [];
-var option = rootCommand.AddDefaultGlobalOptions();
-rootCommand
-    .AddDefaultCommand(option)
-    .AddParseCommand(option);
-return await rootCommand.Parse(args).InvokeAsync();
-
+CancellationTokenSource cancellationTokenSource = new();
+void CancelKeyPress(object? _, ConsoleCancelEventArgs e)
+{
+    e.Cancel = true;
+    cancellationTokenSource.Cancel();
+}
+try {
+    Console.CancelKeyPress += CancelKeyPress;
+    return await RunAsync(args, cancellationTokenSource.Token);
+} finally
+{
+    Console.CancelKeyPress -= CancelKeyPress;
+}
 
 /// <summary>
 /// The main program class for the Brainfuck interpreter. This class is responsible for setting up the command-line interface and handling user input. It defines the entry point of the application and orchestrates the execution of commands based on the provided arguments.
 /// </summary>
-public partial class Program { }
+internal partial class Program {
+    public static async Task<int> RunAsync(string[] args, CancellationToken cancellationToken)
+    {
+        RootCommand rootCommand = [];
+        var option = rootCommand.AddDefaultGlobalOptions();
+        rootCommand
+            .AddDefaultCommand(option)
+            .AddParseCommand(option);
+        return await rootCommand.Parse(args).InvokeAsync(cancellationToken: cancellationToken);
+
+    }
+}
